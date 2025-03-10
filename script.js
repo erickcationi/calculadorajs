c5cd74553538a9fe58a7fa132aa02608534630f2
Erick Cationi Cardoso <erickcationicardosoo@gmail.com>
2025-03-10 09:19:12 -0300

    12

---
+++ script.js
@@ -0,0 +1,11 @@
+const calculator = document.querySelector(‘.calculator’)
+const keys = calculator.querySelector(‘.calculator__keys’)
+
+keys.addEventListener(‘click’, e => {
+ if (e.target.matches(‘button’)) {
+   // Fazer algo
+ }
+})
+
+const key = e.target
+const action = key.dataset.action
